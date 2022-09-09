1ba4b6aa708804fb0dba1e0573e568f72458480c
Louis Perdomo <louis.perdomo79@gmail.com>
2022-09-09 17:00:51 -0500

    endpoint onboarding

--- app/src/main/java/com/quickstore/data/RestConstant.kt
+++ app/src/main/java/com/quickstore/data/RestConstant.kt
@@ -16,30 +16,26 @@ object RestConstant {
     const val PRODUCT = "product%2F"
     const val CATEGORY = "category%2F"
 
-    private const val V1_API = "/api/v1"
-    private const val PUBLIC_API = "$V1_API/public"
-    private const val SECURED_API = "$V1_API/secured"
+    const val ENDPOINT_ON_BOARDING = "/onboarding"
 
-    const val ENDPOINT_ON_BOARDING = "$PUBLIC_API/slider/all"
+    const val ENDPOINT_CART = "/cart"
+    const val ENDPOINT_CART_DECREASE = "/decrease"
 
-    const val ENDPOINT_CART = "$SECURED_API/cart"
-    const val ENDPOINT_CART_DECREASE = "$ENDPOINT_CART/decrease"
-
-    const val ENDPOINT_CTEGORY = "$PUBLIC_API/category"
+    const val ENDPOINT_CTEGORY = "/category"
 
     const val ENDPOINT_TOKEN = "/oauth/token"
-    const val ENDPOINT_USER = "$SECURED_API/user"
-    const val ENDPOINT_USER_INFO = "$ENDPOINT_USER/me"
-    const val ENDPOINT_USER_CHANGE_PASS = "$ENDPOINT_USER/changepass"
-    const val ENDPOINT_USER_PUBLIC = "$PUBLIC_API/user"
-    const val ENDPOINT_USER_RECOVER_PASS = "$ENDPOINT_USER_PUBLIC/recoverpass"
+    const val ENDPOINT_USER = "/user"
+    const val ENDPOINT_USER_INFO = "/me"
+    const val ENDPOINT_USER_CHANGE_PASS = "/changepass"
+    const val ENDPOINT_USER_PUBLIC = "/user"
+    const val ENDPOINT_USER_RECOVER_PASS = "/recoverpass"
 
-    const val ENDPOINT_ADDRESS = "$SECURED_API/address"
-    const val ENDPOINT_DISTRICT = "$PUBLIC_API/district"
+    const val ENDPOINT_ADDRESS = "/address"
+    const val ENDPOINT_DISTRICT = "/district"
 
-    const val ENDPOINT_FIREBASE_TOKEN = "$SECURED_API/user_token"
+    const val ENDPOINT_FIREBASE_TOKEN = "/user_token"
 
-    const val ENDPOINT_PRODUCT = "$PUBLIC_API/product"
+    const val ENDPOINT_PRODUCT = "/product"
 
     class Credentials {
         var authCredentials: String = BASIC + Base64.encodeToString(
