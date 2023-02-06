a1dc2c841f5eec18bb4377ac8506c7243c93795f
Louis Perdomo <louis.perdomo79@gmail.com>
2023-02-05 20:04:53 -0400

    App completed

--- app/src/main/java/com/quickstore/data/cart/CartWebServices.kt
+++ app/src/main/java/com/quickstore/data/cart/CartWebServices.kt
@@ -7,22 +7,21 @@ import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.*
 
-const val PRODUCT_ID = "productId"
 interface CartWebServices {
 
     @POST(RestConstant.ENDPOINT_ADD_CART)
     fun addCart(@Header("Authorization") token:  String,
                 @Body request: AddCartRequest): Call<Void>
 
-    @POST(RestConstant.ENDPOINT_DECREASE_CART)
+    @POST(RestConstant.ENDPOINT_DECREASE_CART + "/{id}")
     fun decreaseCart(@Header("Authorization") token:  String,
-                @Query(PRODUCT_ID) productId: Long): Call<Void>
+                     @Path("id") idProduct: Long): Call<Void>
 
     @GET(RestConstant.ENDPOINT_LIST_CART)
     fun listCart(@Header("Authorization") token:  String): Observable<CartModel>
 
-    @DELETE(RestConstant.ENDPOINT_DELETE_PRODUCT_CART)
+    @DELETE(RestConstant.ENDPOINT_DELETE_PRODUCT_CART  + "/{id}")
     fun deleteProductCart(@Header("Authorization") token:  String,
-                          @Query(PRODUCT_ID) productId: Long): Call<Void>
+                          @Path("id") idProduct: Long): Call<Void>
 
 }
